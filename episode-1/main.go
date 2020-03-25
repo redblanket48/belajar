b4b21011db3472b30ed6fbc7e2fd47e64f6a06cc
joe <redblanket48@gmail.com>
2020-03-25 16:22:01 +0700

    first episode

---
+++ episode-1/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Product holds your product attribute
+type Product struct {
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	Price    int    `json:"price"`
+	Quantity int    `json:"quantity"`
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Selamat datang di home page")
+}
+
+func allProducts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	json.NewEncoder(w).Encode(Products)
+}
+
+func singleProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	params := mux.Vars(r)
+	id := params["id"]
+
+	for _, product := range Products {
+		if product.ID == id {
+			json.NewEncoder(w).Encode(product)
+			return
+		}
+	}
+}
+
+func createProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	var product Product
+
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	Products = append(Products, product)
+	json.NewEncoder(w).Encode(product)
+}
+
+func updateProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	params := mux.Vars(r)
+	id := params["id"]
+
+	var product Product
+
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	for i, p := range Products {
+		if p.ID == id {
+			Products[i].Title = product.Title
+			Products[i].Price = product.Price
+			Products[i].Quantity = product.Quantity
+			json.NewEncoder(w).Encode(Products[i])
+			return
+		}
+	}
+}
+
+func deleteProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	params := mux.Vars(r)
+	id := params["id"]
+
+	for i, p := range Products {
+		if p.ID == id {
+			Products = append(Products[:i], Products[i+1:]...)
+			json.NewEncoder(w).Encode(p)
+			return
+		}
+	}
+}
+
+func handleRequest() {
+	r := mux.NewRouter().StrictSlash(true)
+
+	r.HandleFunc("/", home)
+	r.HandleFunc("/products", allProducts).Methods("GET")
+	r.HandleFunc("/products/{id}", singleProduct).Methods("GET")
+	r.HandleFunc("/products", createProduct).Methods("POST")
+	r.HandleFunc("/products/{id}", updateProduct).Methods("PUT")
+	r.HandleFunc("/products/{id}", deleteProduct).Methods("DELETE")
+
+	fmt.Println("Application running")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func main() {
+	Products = []Product{
+		Product{ID: "1", Title: "First Product", Price: 200000, Quantity: 5},
+		Product{ID: "2", Title: "Second Product", Price: 500000, Quantity: 15},
+	}
+
+	handleRequest()
+}
+
+// Products is a global variable to hold collection of products
+var Products []Product
